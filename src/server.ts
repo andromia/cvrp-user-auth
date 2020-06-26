d3c6f6c5c0a2723583cc5b3a4f9990a3167e3d4d
Phillip Rognerud <philliprognerud2@yahoo.com>
2020-06-25 17:34:14 -0700

    added mongo

--- src/server.ts
+++ src/server.ts
@@ -5,24 +5,19 @@ import cors from "cors";
 // Setup Mongoose connection
 const options = {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  autoIndex: false, // Don't build indexes
-  poolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
 };
 
-mongoose.connection.on("open", function (ref) {
-  console.log("Connected to mongo server.");
+mongoose.connection.on("open", (ref) => {
+  console.log("User auth connected to Mongo!");
 });
 
-mongoose.connection.on("error", function (err) {
-  console.log("Could not connect to mongo server!");
+mongoose.connection.on("error", (err) => {
+  console.log("User auth IS NOT connected to Mongo!");
 });
 
-let mongoURI = "mongodb://mongo:27017/auth";
+let mongoURI = "mongodb://mongo:27017/api/user";
 mongoose.connect(mongoURI, options);
 
 // Boot express
