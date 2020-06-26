ee6b4104d71d2966f4d20907a8af0c546ef75ba8
Phillip Rognerud <philliprognerud2@yahoo.com>
2020-06-25 17:11:05 -0700

    added mongo

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+
+// Setup Mongoose connection
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+};
+
+mongoose.connect("mongodb://localhost:27017", options).catch((error) => {
+  console.log(error);
+});
 
 // Boot express
 const app: Application = express();
 app.use(cors());
-
-const port = 8080;
 
 // Application routing
 app.use("/", (req: Request, res: Response, next: NextFunction) => {
@@ -13,6 +28,7 @@ app.use("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 // Start server
-app.listen(port, () =>
-  console.log(`User auth microservice listening on port: ${port}!`)
+const PORT = 8080;
+app.listen(PORT, () =>
+  console.log(`User auth microservice listening on port: ${PORT}!`)
 );
