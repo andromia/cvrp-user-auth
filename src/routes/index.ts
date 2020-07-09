e959e02de51e16e356dcd77362bcfbbc13c8c396
chris pryer <christophpryer@gmail.com>
2020-07-08 23:53:01 -0400

    updated index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import config from "config";
 // Import Controllers
 import localAuth from "./controllers/local-auth";
 import githubAuth from "./controllers/github-auth";
+import googleAuth from "./controllers/google-auth";
 import logout from "./controllers/logout";
 
 export default (app: Application) => {
     app.use(config.get("BASE_PATH") + localAuth.path, localAuth.router);
     app.use(config.get("BASE_PATH") + githubAuth.path, githubAuth.router);
+    app.use(config.get("BASE_PATH") + googleAuth.path, googleAuth.router);
     app.use(config.get("BASE_PATH") + logout.path, logout.router);
 };
